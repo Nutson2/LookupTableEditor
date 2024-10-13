e678a79130bf0597d3cf3d6ba1f41b4fc6d2c03b
Алексей Орешкин <Nutson@outlook.com>
2024-10-13 09:55:23 +0300

    удалил лишнее

--- LookupTableEditor/Extentions/ObjectExtention.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LookupTableEditor.Extentions
-{
-    public static class ObjectExtention
-    {
-        public static T ThrowIfNull<T>(this T obj)
-            where T : class => obj ?? throw new CustomNullException($"{typeof(T)} is null");
-    }
-}
