7baa56ff5eb01b9e7dd91176c3e449c16a90bbb8
Алексей Орешкин <Nutson@outlook.com>
2025-05-01 15:43:46 +0300

    add request table name dialog

---
+++ LookupTableEditor/Views/Pages/RequestTableName.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using LookupTableEditor.ViewModels;
+
+namespace LookupTableEditor.Views.Pages;
+
+/// <summary>
+/// Логика взаимодействия для RequestTableName.xaml
+/// </summary>
+public partial class RequestTableName : Page
+{
+    public RequestTableName(RequestTableNameVM vm)
+    {
+        InitializeComponent();
+        DataContext = vm;
+    }
+}
