9ab456319d888be6b51c29b4c60447bfe3562bdd
Алексей Орешкин <Nutson@outlook.com>
2025-05-01 15:43:17 +0300

    add dictionary extentions

---
+++ LookupTableEditor/Extentions/DictionaryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LookupTableEditor.Extentions;
+
+public static class DictionaryExtensions
+{
+    public static TValue? GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+    {
+        if (dict.TryGetValue(key, out var res))
+        {
+            return res;
+        }
+        return default;
+    }
+
+    public static void TryAdd<TKey, TValue>(
+        this Dictionary<TKey, TValue> dict,
+        TKey key,
+        TValue value
+    )
+    {
+        if (!dict.ContainsKey(key))
+        {
+            dict.Add(key, value);
+        }
+    }
+}
