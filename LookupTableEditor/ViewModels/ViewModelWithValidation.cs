6fe01598f7abb9b1f1b3e60ba22464ba700a9434
Алексей Орешкин <Nutson@outlook.com>
2025-05-01 15:43:01 +0300

    add base vm validation

---
+++ LookupTableEditor/ViewModels/ViewModelWithValidation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LookupTableEditor.Extentions;
+
+namespace LookupTableEditor.ViewModels;
+
+public class ErrorsViewModel : ObservableObject, INotifyDataErrorInfo
+{
+    private readonly Dictionary<string, List<string>> _propertyErrors = new();
+    public bool HasErrors => _propertyErrors.Any();
+
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    public IEnumerable? GetErrors(string propertyName) =>
+        _propertyErrors.GetOrDefault(propertyName) ?? Enumerable.Empty<string>();
+
+    private void AddError(string propertyName, string errorMessage)
+    {
+        _propertyErrors.TryAdd(propertyName, new());
+        _propertyErrors[propertyName].Add(errorMessage);
+        OnErrorsChanged(propertyName);
+    }
+
+    private void ClearErrors(string propertyName)
+    {
+        if (_propertyErrors.Remove(propertyName))
+        {
+            OnErrorsChanged(propertyName);
+        }
+    }
+
+    private void OnErrorsChanged(string propertyName) =>
+        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+
+    public void Validate(
+        Func<bool> predicat,
+        string warningMessage,
+        [CallerMemberName] string propertyName = ""
+    )
+    {
+        ClearErrors(propertyName);
+        if (!predicat.Invoke())
+        {
+            AddError(propertyName, warningMessage);
+        }
+    }
+}
