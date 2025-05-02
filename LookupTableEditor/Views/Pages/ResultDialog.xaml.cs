9817853186c4d1da7df27f93f94aad93c9c97c84
Алексей Орешкин <Nutson@outlook.com>
2025-05-02 19:37:18 +0300

    add result dialog

---
+++ LookupTableEditor/Views/Pages/ResultDialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using LookupTableEditor.ViewModels;
+
+namespace LookupTableEditor.Views.Pages;
+
+public partial class ResultDialog : Page
+{
+    public ResultDialog(ResultVM resultVM)
+    {
+        InitializeComponent();
+        DataContext = resultVM;
+    }
+}
