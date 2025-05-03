b0bc2d85722b87096bafeffde2c2bbbd6d3c3629
Алексей Орешкин <Nutson@outlook.com>
2025-05-03 19:26:21 +0300

    edit new table name validation

--- LookupTableEditor/ViewModels/RequestTableNameVM.cs
+++ LookupTableEditor/ViewModels/RequestTableNameVM.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using LookupTableEditor.Extentions;
 
 namespace LookupTableEditor.ViewModels;
 
 public partial class RequestTableNameVM : BaseDialogVM<string>
 {
-    public RequestTableNameVM(BaseViewModel ownerVM, Action<string?> action)
-        : base(ownerVM, action)
-    {
-        RequestVal = string.Empty;
-    }
+	public RequestTableNameVM(BaseViewModel ownerVM, Action<string?> action)
+		: base(ownerVM, action)
+	{
+		RequestVal = string.Empty;
+	}
 
-    public override void ValidateRequestedProp(string? value) =>
-        Validate(() => value?.IsValid() == true, "Не может быть пустым.", nameof(RequestVal));
+	public override void ValidateRequestedProp(string? value) =>
+		Validate(
+			() =>
+			{
+				if (value?.IsValid() == false)
+				{
+					return "Не может быть пустым.";
+				}
+				var invalidChar = Path.GetInvalidFileNameChars().Where(c => value.Contains(c));
+				if (invalidChar.Any())
+				{
+					return $"Наименование таблицы не может содержать символы: {string.Join(", ", invalidChar)}";
+				}
+				return null;
+			},
+			nameof(RequestVal)
+		);
 }
--- LookupTableEditor/ViewModels/ViewModelWithValidation.cs
+++ LookupTableEditor/ViewModels/ViewModelWithValidation.cs
@@ -11,44 +11,55 @@ namespace LookupTableEditor.ViewModels;
 
 public class ErrorsViewModel : ObservableObject, INotifyDataErrorInfo
 {
-    private readonly Dictionary<string, List<string>> _propertyErrors = new();
-    public bool HasErrors => _propertyErrors.Any();
+	private readonly Dictionary<string, List<string>> _propertyErrors = new();
+	public bool HasErrors => _propertyErrors.Any();
 
-    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+	public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
 
-    public IEnumerable GetErrors(string? propertyName)
-    {
-        if (propertyName is null)
-            Enumerable.Empty<string>();
+	public IEnumerable GetErrors(string? propertyName)
+	{
+		if (propertyName is null)
+			Enumerable.Empty<string>();
 
-        return _propertyErrors.GetOrDefault(propertyName!) ?? Enumerable.Empty<string>();
-    }
+		return _propertyErrors.GetOrDefault(propertyName!) ?? Enumerable.Empty<string>();
+	}
 
-    private void AddError(string propertyName, string errorMessage)
-    {
-        _propertyErrors.TryAdd(propertyName, new());
-        _propertyErrors[propertyName].Add(errorMessage);
-    }
+	private void AddError(string propertyName, string errorMessage)
+	{
+		_propertyErrors.TryAdd(propertyName, new());
+		_propertyErrors[propertyName].Add(errorMessage);
+	}
 
-    private void ClearErrors(string propertyName) => _propertyErrors.Remove(propertyName);
+	private void ClearErrors(string propertyName) => _propertyErrors.Remove(propertyName);
 
-    private void OnErrorsChanged(string propertyName)
-    {
-        OnPropertyChanged(nameof(HasErrors));
-        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-    }
+	private void OnErrorsChanged(string propertyName)
+	{
+		OnPropertyChanged(nameof(HasErrors));
+		ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+	}
 
-    public void Validate(
-        Func<bool> predicat,
-        string warningMessage,
-        [CallerMemberName] string propertyName = ""
-    )
-    {
-        ClearErrors(propertyName);
-        if (!predicat.Invoke())
-        {
-            AddError(propertyName, warningMessage);
-        }
-        OnErrorsChanged(propertyName);
-    }
+	public void Validate(
+		Func<bool> predicat,
+		string warningMessage,
+		[CallerMemberName] string propertyName = ""
+	)
+	{
+		ClearErrors(propertyName);
+		if (!predicat.Invoke())
+		{
+			AddError(propertyName, warningMessage);
+		}
+		OnErrorsChanged(propertyName);
+	}
+
+	public void Validate(Func<string?> predicat, [CallerMemberName] string propertyName = "")
+	{
+		ClearErrors(propertyName);
+		var msg = predicat.Invoke();
+		if (msg is not null)
+		{
+			AddError(propertyName, msg);
+		}
+		OnErrorsChanged(propertyName);
+	}
 }
