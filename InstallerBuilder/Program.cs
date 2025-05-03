7ed64e18dc700af67cfb7861012ca18787c8f585
Алексей Орешкин <Nutson@outlook.com>
2025-05-03 13:26:57 +0300

    add inctaller project

---
+++ InstallerBuilder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WixSharp;
+using File = WixSharp.File;
+
+internal class Program
+{
+    private const string baseBundlePath =
+        @"C:\ProgramData\Autodesk\ApplicationPlugins\LookupTableEditor.bundle";
+
+    private static void Main(string[] args)
+    {
+        string solutionPath = args[0];
+
+        string path = Path.Combine(solutionPath, @"LookupTableEditor\bin");
+
+        var dir = new DirectoryInfo(path);
+        var dirs = new List<Dir>();
+        foreach (var item in dir.GetDirectories().Where(d => d.Name.Contains("Release")))
+        {
+            var dir2 = GetDir(item);
+
+            Dir[] sub = item.GetDirectories().Select(d => GetDir(d)).ToArray();
+
+            dir2.Dirs = dir2.Dirs.Concat(sub).ToArray();
+            dirs.Add(dir2);
+        }
+
+        string packageContents = Path.Combine(
+            solutionPath,
+            @"InstallerBuilder\PackageContents.xml"
+        );
+        var contentXml = new File(packageContents);
+
+        var project = new Project(
+            "LookupTableEditor",
+            new Dir(baseBundlePath, contentXml, new Dir("Contents", dirs.ToArray()))
+        );
+        project.UI = WUI.WixUI_ProgressOnly;
+        project.BuildMsi(
+            Path.Combine(solutionPath, @"LookupTableEditor\bin\LookupTableEditor installer")
+        );
+    }
+
+    private static Dir GetDir(DirectoryInfo? item)
+    {
+        return new Dir(item!.Name, item.GetFiles().Select(f => new File(f.FullName)).ToArray());
+    }
+}
--- build/Build.cs
+++ build/Build.cs
@@ -1,40 +1,35 @@
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-class Build2 : NukeBuild
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
 {
-    public static int Main2() => Execute<Build2>(x => x.AfterCompile);
+    public static int Main() => Execute<Build>(x => x.CreateMSI);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild
-        ? Configuration.Debug
-        : Configuration.Release;
-
-    [Solution]
+    [Solution(GenerateProjects = true)]
     readonly Solution Solution;
-    AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    AbsolutePath SourceDirectory => RootDirectory;
+
+    Project LookupTableEditor => Solution.GetProject("LookupTableEditor");
 
     Target Clean =>
         _ =>
             _.Before(Restore)
                 .Executes(() =>
                 {
-                    Solution
-                        .Projects.Where(p => p.Name != "_build")
-                        .SelectMany(p => p.Directory.GlobDirectories("**/bin", "**/obj"))
-                        .ForEach(c =>
-                        {
-                            Log.Information($"Delete directory: {c.Parent}\\{c.Name}");
-                            c.DeleteDirectory();
-                        });
+                    SourceDirectory
+                        .GlobDirectories("**/bin", "**/obj")
+                        .Where(x => !x.ToString().Contains("build"))
+                        .ForEach(i => i.DeleteDirectory());
                 });
 
     Target Restore =>
@@ -58,25 +53,39 @@ class Build2 : NukeBuild
                         DotNetBuild(s => s.SetProjectFile(Solution).SetConfiguration(t));
                     }
                 });
-    Target AfterCompile =>
+
+    Target CopyAddInFiles =>
         _ =>
             _.DependsOn(Compile)
+                .OnlyWhenDynamic(() => LookupTableEditor != null)
                 .Executes(() =>
                 {
-                    var t = Solution.Projects.FirstOrDefault(p => p.Name == "LookupTableEditor");
-                    var addin = Path.Combine(
-                        Path.GetDirectoryName(t.Path),
-                        "LookupTableEditor.addin"
-                    );
-                    var addinFile = new FileInfo(addin);
-                    var p = t.Directory.GlobDirectories("**/bin").First();
+                    var addinFile = LookupTableEditor.Directory / "LookupTableEditor.addin";
+                    if (!addinFile.FileExists())
+                        return;
 
-                    foreach (var item in Directory.GetDirectories(p))
-                    {
-                        File.Copy(
-                            addinFile.FullName,
-                            Path.Combine(item, "LookupTableEditor.addin")
-                        );
-                    }
+                    LookupTableEditor
+                        .Directory.GlobDirectories("**/bin")
+                        .SelectMany(d => d.GlobDirectories("*"))
+                        .Where(d => !d.Name.Contains("Debug"))
+                        .ForEach(d =>
+                        {
+                            addinFile.CopyToDirectory(
+                                d,
+                                ExistsPolicy.FileOverwrite,
+                                createDirectories: true
+                            );
+                        });
+                });
+    Target CreateMSI =>
+        _ =>
+            _.DependsOn(CopyAddInFiles)
+                .Executes(() =>
+                {
+                    var projPath = Solution
+                        .AllProjects.FirstOrDefault(p => p.Name == "InstallerBuilder")
+                        .Directory;
+                    var exe = Path.Combine(projPath, "bin", "Release", "InstallerBuilder.exe");
+                    Process.Start(exe, Solution.Directory);
                 });
 }
