54072694e02498e96006a26dcc4ca8dc8e7a16bd
Алексей Орешкин <Nutson@outlook.com>
2025-05-04 11:49:23 +0300

    explicit encoding using on save file

--- LookupTableEditor/Services/SizeTableService.cs
+++ LookupTableEditor/Services/SizeTableService.cs
@@ -13,172 +13,174 @@ namespace LookupTableEditor.Services;
 
 public class SizeTableService : IDisposable
 {
-	private readonly RevitApplication _app;
-	private readonly Document _doc;
-	private readonly AbstractParameterTypesProvider _parameterTypesProvider;
-	public FamilySizeTableManager Manager { get; }
-	public List<AbstractParameterType> AbstractParameterTypes { get; }
+    static Encoding win1251 = Encoding.GetEncoding("Windows-1251");
 
-	public SizeTableService(
-		Document doc,
-		RevitApplication application,
-		AbstractParameterTypesProvider parameterTypesProvider
-	)
-	{
-		_doc = doc ?? throw new ArgumentNullException(nameof(doc));
-		_app = application ?? throw new ArgumentNullException(nameof(application));
-		_parameterTypesProvider =
-			parameterTypesProvider
-			?? throw new ArgumentNullException(nameof(parameterTypesProvider));
+    private readonly RevitApplication _app;
+    private readonly Document _doc;
+    private readonly AbstractParameterTypesProvider _parameterTypesProvider;
+    public FamilySizeTableManager Manager { get; }
+    public List<AbstractParameterType> AbstractParameterTypes { get; }
 
-		Manager = GetOrCreateSizeTableManager();
+    public SizeTableService(
+        Document doc,
+        RevitApplication application,
+        AbstractParameterTypesProvider parameterTypesProvider
+    )
+    {
+        _doc = doc ?? throw new ArgumentNullException(nameof(doc));
+        _app = application ?? throw new ArgumentNullException(nameof(application));
+        _parameterTypesProvider =
+            parameterTypesProvider
+            ?? throw new ArgumentNullException(nameof(parameterTypesProvider));
 
-		var defs = GetDefenitionsOfParameterType();
+        Manager = GetOrCreateSizeTableManager();
 
-		AbstractParameterTypes = defs.ConvertAll(def =>
-			_parameterTypesProvider.FromDefinitionOfParameterType(def)
-		);
-	}
+        var defs = GetDefenitionsOfParameterType();
 
-	public void Dispose()
-	{
-		Manager.Dispose();
-	}
+        AbstractParameterTypes = defs.ConvertAll(def =>
+            _parameterTypesProvider.FromDefinitionOfParameterType(def)
+        );
+    }
 
-	private List<DefinitionOfParameterType> GetDefenitionsOfParameterType()
-	{
-		var parametersTypes = _app.VersionNumber.ToInt() switch
-		{
-			2020 => Resource.ParametersTypes2020,
-			2021 => Resource.ParametersTypes2021,
-			2022 => Resource.ParametersTypes2022,
-			2023 => Resource.ParametersTypes2023,
-			2024 => Resource.ParametersTypes2024,
-			_ => Resource.ParametersTypes2024,
-		};
-		using MemoryStream stream = new(parametersTypes);
-		XmlSerializer xmlSerializer = new(typeof(List<DefinitionOfParameterType>));
-		return (List<DefinitionOfParameterType>)xmlSerializer.Deserialize(stream)!;
-	}
+    public void Dispose()
+    {
+        Manager.Dispose();
+    }
 
-	private FamilySizeTableManager GetOrCreateSizeTableManager()
-	{
-		_doc.Run(
-			"Create manager",
-			() => FamilySizeTableManager.CreateFamilySizeTableManager(_doc, _doc.OwnerFamily.Id)
-		);
-		return FamilySizeTableManager.GetFamilySizeTableManager(_doc, _doc.OwnerFamily.Id)
-			?? throw new NullReferenceException(
-				$"{nameof(FamilySizeTableManager.GetFamilySizeTableManager)} return null\n"
-			);
-	}
+    private List<DefinitionOfParameterType> GetDefenitionsOfParameterType()
+    {
+        var parametersTypes = _app.VersionNumber.ToInt() switch
+        {
+            2020 => Resource.ParametersTypes2020,
+            2021 => Resource.ParametersTypes2021,
+            2022 => Resource.ParametersTypes2022,
+            2023 => Resource.ParametersTypes2023,
+            2024 => Resource.ParametersTypes2024,
+            _ => Resource.ParametersTypes2024,
+        };
+        using MemoryStream stream = new(parametersTypes);
+        XmlSerializer xmlSerializer = new(typeof(List<DefinitionOfParameterType>));
+        return (List<DefinitionOfParameterType>)xmlSerializer.Deserialize(stream)!;
+    }
 
-	public SizeTableInfo GetSizeTableInfo(string name)
-	{
-		SizeTableInfo sizeTableInfo = new(name, AbstractParameterTypes, _parameterTypesProvider);
-		var familySizeTable = Manager.GetSizeTable(name);
+    private FamilySizeTableManager GetOrCreateSizeTableManager()
+    {
+        _doc.Run(
+            "Create manager",
+            () => FamilySizeTableManager.CreateFamilySizeTableManager(_doc, _doc.OwnerFamily.Id)
+        );
+        return FamilySizeTableManager.GetFamilySizeTableManager(_doc, _doc.OwnerFamily.Id)
+            ?? throw new NullReferenceException(
+                $"{nameof(FamilySizeTableManager.GetFamilySizeTableManager)} return null\n"
+            );
+    }
 
-		sizeTableInfo.InsertFirstColumn();
+    public SizeTableInfo GetSizeTableInfo(string name)
+    {
+        SizeTableInfo sizeTableInfo = new(name, AbstractParameterTypes, _parameterTypesProvider);
+        var familySizeTable = Manager.GetSizeTable(name);
 
-		if (familySizeTable == null)
-			return sizeTableInfo;
+        sizeTableInfo.InsertFirstColumn();
 
-		for (var columnIndx = 1 ; columnIndx < familySizeTable.NumberOfColumns ; columnIndx++)
-		{
-			var column = familySizeTable.GetColumnHeader(columnIndx);
-			sizeTableInfo.AddHeader(column);
-		}
+        if (familySizeTable == null)
+            return sizeTableInfo;
 
-		for (var rowIndex = 0 ; rowIndex < familySizeTable.NumberOfRows ; rowIndex++)
-		{
-			var row = sizeTableInfo.Table.NewRow();
-			sizeTableInfo.Table.Rows.Add(row);
+        for (var columnIndx = 1; columnIndx < familySizeTable.NumberOfColumns; columnIndx++)
+        {
+            var column = familySizeTable.GetColumnHeader(columnIndx);
+            sizeTableInfo.AddHeader(column);
+        }
 
-			for (var columnIndex = 0 ; columnIndex < familySizeTable.NumberOfColumns ; columnIndex++)
-			{
-				var val = familySizeTable.AsValueString(rowIndex, columnIndex);
+        for (var rowIndex = 0; rowIndex < familySizeTable.NumberOfRows; rowIndex++)
+        {
+            var row = sizeTableInfo.Table.NewRow();
+            sizeTableInfo.Table.Rows.Add(row);
 
-				var dataColumn = sizeTableInfo.Table.Columns[columnIndex];
+            for (var columnIndex = 0; columnIndex < familySizeTable.NumberOfColumns; columnIndex++)
+            {
+                var val = familySizeTable.AsValueString(rowIndex, columnIndex);
 
-				row[dataColumn.ColumnName] =
-					dataColumn.DataType == typeof(double) ? val.ToDouble() : val;
-			}
-		}
+                var dataColumn = sizeTableInfo.Table.Columns[columnIndex];
 
-		return sizeTableInfo;
-	}
+                row[dataColumn.ColumnName] =
+                    dataColumn.DataType == typeof(double) ? val.ToDouble() : val;
+            }
+        }
 
-	public string? ImportSizeTable(SizeTableInfo tableInfo)
-	{
-		FamilySizeTableErrorInfo errorInfo = new();
+        return sizeTableInfo;
+    }
 
-		_doc.Run(
-			"Set size table",
-			() =>
-			{
-				if (Manager.HasSizeTable(tableInfo.Name))
-					Manager.RemoveSizeTable(tableInfo.Name);
+    public string? ImportSizeTable(SizeTableInfo tableInfo)
+    {
+        FamilySizeTableErrorInfo errorInfo = new();
 
-				Manager.ImportSizeTable(_doc, tableInfo.FilePath, errorInfo);
-			}
-		);
+        _doc.Run(
+            "Set size table",
+            () =>
+            {
+                if (Manager.HasSizeTable(tableInfo.Name))
+                    Manager.RemoveSizeTable(tableInfo.Name);
 
-		if (errorInfo.FamilySizeTableErrorType == FamilySizeTableErrorType.Undefined)
-		{
-			return null;
-		}
-		return $"Проблема импорта таблицы:\n"
-			+ $"{errorInfo.FamilySizeTableErrorType}\n"
-			+ $"{errorInfo.InvalidHeaderText}\n"
-			+ $"{errorInfo.InvalidColumnIndex}\n"
-			+ $"{errorInfo.InvalidRowIndex}";
-	}
+                Manager.ImportSizeTable(_doc, tableInfo.FilePath, errorInfo);
+            }
+        );
 
-	public void SaveSizeTableOnTheDisk(SizeTableInfo tableInfo)
-	{
-		var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        if (errorInfo.FamilySizeTableErrorType == FamilySizeTableErrorType.Undefined)
+        {
+            return null;
+        }
+        return $"Проблема импорта таблицы:\n"
+            + $"{errorInfo.FamilySizeTableErrorType}\n"
+            + $"{errorInfo.InvalidHeaderText}\n"
+            + $"{errorInfo.InvalidColumnIndex}\n"
+            + $"{errorInfo.InvalidRowIndex}";
+    }
 
-		tableInfo.FilePath = Path.Combine(folderPath, tableInfo.Name + ".csv");
+    public void SaveSizeTableOnTheDisk(SizeTableInfo tableInfo)
+    {
+        var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-		using StreamWriter sw = new(tableInfo.FilePath, false, Encoding.Default);
-		sw.Write(tableInfo.ConvertToString());
-	}
+        tableInfo.FilePath = Path.Combine(folderPath, tableInfo.Name + ".csv");
 
-	public (SizeTableInfo?, string?) Update(SizeTableInfo tableInfo)
-	{
-		try
-		{
-			SaveSizeTableOnTheDisk(tableInfo);
-			var res = ImportSizeTable(tableInfo);
-			if (res is not null)
-				return (null, res);
-			tableInfo = GetSizeTableInfo(tableInfo.Name!);
-			return (tableInfo, null);
-		}
-		catch (Exception ex)
-		{
-			return (null, $"Возникла проблема:\n{ex.Message}");
-		}
-	}
+        using StreamWriter sw = new(tableInfo.FilePath, false, win1251);
+        sw.Write(tableInfo.ConvertToString());
+    }
 
-	public string CreateFormula(
-		FamilyParameter parameter,
-		SizeTableInfo tableInfo,
-		List<FamilyParameter> keyParameters
-	)
-	{
-		if (parameter.Formula != null)
-			return parameter.Formula;
+    public (SizeTableInfo?, string?) Update(SizeTableInfo tableInfo)
+    {
+        try
+        {
+            SaveSizeTableOnTheDisk(tableInfo);
+            var res = ImportSizeTable(tableInfo);
+            if (res is not null)
+                return (null, res);
+            tableInfo = GetSizeTableInfo(tableInfo.Name!);
+            return (tableInfo, null);
+        }
+        catch (Exception ex)
+        {
+            return (null, $"Возникла проблема:\n{ex.Message}");
+        }
+    }
 
-		var tableName = $"\"{tableInfo.Name}\"";
-		var columnName = parameter.Definition.Name;
-		var defaultValue =
-			tableInfo.Table.Rows[0][columnName.Replace(".", "_")].ToString()?.Replace("\"", "")
-			?? string.Empty;
+    public string CreateFormula(
+        FamilyParameter parameter,
+        SizeTableInfo tableInfo,
+        List<FamilyParameter> keyParameters
+    )
+    {
+        if (parameter.Formula != null)
+            return parameter.Formula;
 
-		var keys = string.Join(", ", keyParameters.Select(x => x.Definition.Name));
+        var tableName = $"\"{tableInfo.Name}\"";
+        var columnName = parameter.Definition.Name;
+        var defaultValue =
+            tableInfo.Table.Rows[0][columnName.Replace(".", "_")].ToString()?.Replace("\"", "")
+            ?? string.Empty;
 
-		var res = $"size_lookup({tableName}, \"{columnName}\", \"{defaultValue}\" {keys})";
-		return res;
-	}
+        var keys = string.Join(", ", keyParameters.Select(x => x.Definition.Name));
+
+        var res = $"size_lookup({tableName}, \"{columnName}\", \"{defaultValue}\" {keys})";
+        return res;
+    }
 }
